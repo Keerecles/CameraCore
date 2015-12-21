346bc73d0e628dba2fa5c68ca5abe2b3a837e503
Keerecles <keerecles@gmail.com>
2015-12-21 18:28:21 +0800

    The D-bus test of CameraCore

--- src/dbus-api.c
+++ src/dbus-api.c
@@ -1,15 +1,10 @@
 #include "Cameradae.h" 
 
-
 extern GDBusNodeInfo *g_mIntrospectionData;
 extern GDBusInterfaceVTable g_mIfaceVTable;
-extern guint g_mNameRequestId;
 extern guint g_mRegistrationId;
 
-extern GQueue *DbustoCommand_queue;
-extern FILE *fp;
-
-//DLT_IMPORT_CONTEXT(Camera_Daemon);
+extern FILE *fpp;
 void CAMERACORE_gst_method_call( GDBusConnection            *connection,
                                       const gchar           *p_sender,
                                       const gchar           *p_object_path,
@@ -19,36 +14,57 @@ void CAMERACORE_gst_method_call( GDBusConnection            *connection,
                                       GDBusMethodInvocation *p_invocation,
                                       gpointer               p_user_data)
 {
-  //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING("[loc:CAMERACORE_gst_method_call]"),DLT_STRING("start."),DLT_STRING("p_sender = "),DLT_STRING(p_sender),DLT_STRING("p_object_path = "),DLT_STRING(p_object_path),DLT_STRING("p_interface_name = "),DLT_STRING(p_interface_name));
-  //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING("[loc:CAMERACORE_gst_method_call]"),DLT_STRING("p_method_name = "),DLT_STRING(p_method_name));
-  CAMERACORE_log(fp,"[CAMERACORE_log]:CAMERACORE_gst_method_call [START]\n");
-  CAMERACORE_log(fp,"[CAMERACORE_log]:In CAMERACORE_gst_method_call [START command_change()]\n");
+  char *p_log[300];
+
+  FILE *fp = fopen("/log.txt", "a+");                      /*创建文件用于输出log*/
+    if(!fp){
+    return 1;
+    }
+    print_log(fpp,"LOOOOOOOOOG\n");
+
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "START the CAMERACORE_gst_method_call\n");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+
   g_dbus_method_invocation_return_value(p_invocation, NULL);
   command_change(p_method_name,p_parameters);
-  CAMERACORE_log(fp,"[CAMERACORE_log]:CAMERACORE_gst_method_call [END]\n");
-  //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING("[loc:CAMERACORE_gst_method_call]"),DLT_STRING("end."));
+  
+   memset(p_log, '\0', 300);
+    sprintf(p_log, "END the CAMERACORE_gst_method_call\n");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
   
   return ;
 }
 
 void CAMERACORE_acquired_bus_cb(GDBusConnection *p_gdus, const gchar *p_name, gpointer p_data)
 {
-  //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING("[loc:CAMERACORE_acquired_bus_cb]"),DLT_STRING("start. p_name = "),DLT_STRING(p_name));
+    char *p_log[300];
+    FILE *fp = fopen("/log.txt", "a+");                      /*创建文件用于输出log*/
+    if(!fp){
+    return 1;
+    }
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "START the CAMERACORE_acquired_bus_cb\n");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+    print_log(fpp,"LOOOOOOOOOG\n");
 
-  CAMERACORE_log(fp,"[CAMERACORE_log]:CAMERACORE_acquired_bus_cb [START]\n");
   memset(&g_mIfaceVTable, 0, sizeof(g_mIfaceVTable));
   g_mIfaceVTable.method_call = CAMERACORE_gst_method_call;
   
   g_mIntrospectionData = g_dbus_node_info_new_for_xml(CAMERADAE_INTERFACE_XML, NULL);
   if(g_mIntrospectionData == NULL){
-    //DLT_LOG(Camera_Daemon,DLT_LOG_ERROR,DLT_STRING("[loc:CAMERACORE_acquired_bus_cb]"),DLT_STRING("[fail g_dbus_node_info_new_for_xml]"));
-    CAMERACORE_log(fp,"[CAMERACORE_log]:CAMERACORE_acquired_bus_cb [Fail g_dbus_node_info_new_for_xml]\n");
+    
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "IN CAMERACORE_acquired_bus_cb: Fail g_dbus_node_info_new_for_xml\n");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
     return;
   }
 
   GError *p_error = NULL;
-  //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING("[loc:CAMERACORE_acquired_bus_cb]"),DLT_STRING("CAMERADAE_OBJ_PATH = "),DLT_STRING(CAMERADAE_OBJ_PATH));
-  
   g_mRegistrationId = g_dbus_connection_register_object( g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL),
                                                          CAMERADAE_OBJ_PATH,
                                                          g_mIntrospectionData->interfaces[0],
@@ -57,65 +73,137 @@ void CAMERACORE_acquired_bus_cb(GDBusConnection *p_gdus, const gchar *p_name, gp
                                                          NULL, //GDestroyNotify
                                                          &p_error);
     if(p_error){
-    //DLT_LOG(Camera_Daemon,DLT_LOG_ERROR,DLT_STRING("[loc:CAMERACORE_acquired_bus_cb]"),DLT_STRING("[fail g_dbus_connection_register_object]"),DLT_STRING(p_error->message));
-    CAMERACORE_log(fp,"[CAMERACORE_log]:CAMERACORE_acquired_bus_cb [Fail g_dbus_connection_register_object]\n");
-    CAMERACORE_log(fp,p_error->message);
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "IN CAMERACORE_acquired_bus_cb: Fail g_dbus_connection_register_object\n");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
     g_error_free(p_error);
     return;
   }
 
-  //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING("[loc:CAMERACORE_acquired_bus_cb]"),DLT_STRING("end."));
-  CAMERACORE_log(fp,"[CAMERACORE_log]:CAMERACORE_acquired_bus_cb [END]\n");
+  memset(p_log, '\0', 300);
+    sprintf(p_log, "END the CAMERACORE_acquired_bus_cb\n");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+  
   return ;
 }
 
 void CAMERACORE_acquired_name_cb(GDBusConnection *p_gdus, const gchar *p_name, gpointer p_data)
 {
-  //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING("[loc:CAMERACORE_acquired_name_cb]"),DLT_STRING("called. p_name = "),DLT_STRING(p_name));
-  CAMERACORE_log(fp,"[CAMERACORE_log]:CAMERACORE_acquired_name_cb [called. p_name =]");
-  CAMERACORE_log(fp,p_name);
-  CAMERACORE_log(fp,"\n");
+    char *p_log[300];
+    FILE *fp = fopen("/log.txt", "a+");                      /*创建文件用于输出log*/
+    if(!fp){
+    return 1;
+    }
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "START CAMERACORE_acquired_name_cb\n");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+        print_log(fpp,"LOOOOOOOOOG\n");
   return ;
 }
 
 void CAMERACORE_lost_name_cb(GDBusConnection *p_gdus, const gchar *p_name, gpointer p_data)
 {
-  CAMERACORE_log(fp,"[CAMERACORE_log]:CAMERACORE_lost_name_cb [called. p_name =]");
-  CAMERACORE_log(fp,p_name);
-  CAMERACORE_log(fp,"\n");
-  //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING("[loc:CAMERACORE_lost_name_cb]"),DLT_STRING("called. p_name = "),DLT_STRING(p_name));
+  
+  char *p_log[300];
+    FILE *fp = fopen("/log.txt", "a+");                      /*创建文件用于输出log*/
+    if(!fp){
+    return 1;
+    }
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "START CAMERACORE_lost_name_cb\n");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+        print_log(fpp,"LOOOOOOOOOG\n");
   return ;
 } 
 
-
-
-
-void command_change(const gchar  *p_method_name, GVariant *parameters){
+void command_change(const gchar *p_method_name, GVariant *parameters){
     
     int _command_ = COMMAND_VIDEO_STOP;
-    CAMERACORE_log(fp,"[CAMERACORE_log]:command_change [START]\n");
-    //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING(" D-bus_api  start."));
-    
-    if(!strcmp(p_method_name,"OpenCamera")){ 
-     
-      g_variant_get(parameters, "(i)", &_command_);
-      g_queue_push_tail(DbustoCommand_queue, _command_);
-
-      if (_command_ == COMMAND_VIDEO_START){
-        CAMERACORE_log(fp,"[CAMERACORE_log]:command_change [Push COMMAND_VIDEO_START into DbustoCommand_queue]\n");
-        //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING(" push COMMAND_VIDEO_START into DbustoCommand_queue"));
-      }
-        else if(_command_ == COMMAND_VIDEO_STOP){
-        CAMERACORE_log(fp,"[CAMERACORE_log]:command_change [Push COMMAND_VIDEO_STOP into DbustoCommand_queue]\n");
-        //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING(" push COMMAND_VIDEO_STOP into DbustoCommand_queue"));
-        }
+    GVariant *status = NULL;
+    char *p_log[300];
+    FILE *fp = fopen("/log.txt", "a+");                      /*创建文件用于输出log*/
+    if(!fp){
+    return 1;
     }
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "START command_change \n");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+
+    
+    // if(!strcmp(p_method_name,"OpenCamera")){ 
+
+    // g_variant_get(parameters, "(v)", &status);
+    // _command_ = g_variant_get_int32(status);
+
+    //   if (_command_ == COMMAND_VIDEO_START){
+       
+    //     g_dbus_connection_emit_signal(g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL),
+    //               TIZEN_PREFIX,   /* the unique bus name for the destination for the signal or NULL to emit to all listeners */
+    //               CAMERADAE_OBJ_PATH,
+    //               CAMERADAE_SERVICE,
+    //               "CameraStatus",
+    //               SIGNAL_CAMERASTATUS_START,
+    //               NULL);
+    //     memset(p_log, '\0', 300);
+    // sprintf(p_log, "In command_change :Send Signal OpenCamera COMMAND_CAMERASTATUS_START \n");
+    // fwrite(p_log, strlen(p_log), 1, fp);
+    // fflush(fp);
+    //   }
+    //     else if(_command_ == COMMAND_VIDEO_STOP){ 
+    //     g_dbus_connection_emit_signal(g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL),
+    //               TIZEN_PREFIX,      /* the unique bus name for the destination for the signal or NULL to emit to all listeners */
+    //               CAMERADAE_OBJ_PATH,
+    //               CAMERADAE_SERVICE,
+    //               "CameraStatus",
+    //               SIGNAL_CAMERASTATUS_STOP,
+    //               NULL);
+    //     memset(p_log, '\0', 300);
+    // sprintf(p_log, "In command_change :Send Signal OpenCamera COMMAND_CAMERASTATUS_STOP \n");
+    // fwrite(p_log, strlen(p_log), 1, fp);
+    // fflush(fp);
+    //     }
+    // }
     
     if(!strcmp(p_method_name,"CapturePicture")){ 
-      g_queue_push_tail(DbustoCommand_queue, COMMAND_CAPTUREPICTURE);
-      CAMERACORE_log(fp,"[CAMERACORE_log]:command_change [Push COMMAND_CAPTUREPICTURE into DbustoCommand_queue]\n");
-      //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING(" push COMMAND_CAPTUREPICTURE into DbustoCommand_queue"));
+
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "method_call name = CapturePicture \n ");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+
+
+    memset(p_log, '\0', 300);
+    sprintf(p_log, " START send signal : g_dbus_connection_emit_signal \n ");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+
+
+      g_dbus_connection_emit_signal(g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL),
+                  NULL,      /* the unique bus name for the destination for the signal or NULL to emit to all listeners */
+                  CAMERADAE_OBJ_PATH,
+                  CAMERADAE_IFACE,
+                  "SnapResult",
+                  g_variant_new("(i)", 1),
+                  NULL);
+
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "In command_change :Send Signal CapturePicture SIGNAL_CAPTUREPICTURE_SUCCESSFUL\n ");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+     memset(p_log, '\0', 300);
+    sprintf(p_log, "In command_change :system gst \n ");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
+    system("gst-launch-1.0 videotestsrc ! autovideosink");
     }
-      CAMERACORE_log(fp,"[CAMERACORE_log]:command_change [END]\n");
-    //DLT_LOG(Camera_Daemon,DLT_LOG_INFO,DLT_STRING("D-bus_api end. "));
+
+    memset(p_log, '\0', 300);
+    sprintf(p_log, "D-bus_api end ");
+    fwrite(p_log, strlen(p_log), 1, fp);
+    fflush(fp);
 }
--- src/ivos-cameradae-service.c
+++ src/ivos-cameradae-service.c
@@ -8,19 +8,11 @@
 #include <syslog.h>
 #include <pthread.h>
 #include <errno.h>
-#include <gst/gst.h>
+//#include <gst/gst.h>
 #include <dbus/dbus.h>
 //#include <dlt/dlt.h>
 #include "Cameradae.h" 
 
-
-//DLT_DECLARE_CONTEXT(Camera_Daemon);
-
-
-void  *Gstreamer_Pipeline(void* v);
-void  *Command_Adapter(void* v);
-void  *FSM(void* v);
-void  *Send_Signal(void* v);
 
 
 GDBusNodeInfo *g_mIntrospectionData=NULL;
@@ -28,16 +20,11 @@ GDBusInterfaceVTable g_mIfaceVTable;
 guint g_mNameRequestId;
 guint g_mRegistrationId;
 
-GQueue *DbustoCommand_queue = NULL;
-GQueue *CommandtoFSM_queue = NULL;
-GQueue *FSMtoGst_queue = NULL;
-GQueue *SingaltoDbus_queue = NULL;
-
 GMainLoop *p_loop = NULL;
-void *retval;
 
-FILE *fp;
-void CAMERACORE_log(FILE *file, char *log_){
+
+FILE *fpp;
+void print_log(FILE *file, const char *log_){
     
     char *p_log[300];
     memset(p_log, '\0', 300);
@@ -46,40 +33,20 @@ void CAMERACORE_log(FILE *file, char *log_){
     fflush(file);
 }
 
-
 int main(int argc, char *argv[]) {
 
-  fp = fopen("/CAMERACORE_log.txt", "a+");                      /*创建文件用于输出log*/
-    if(!fp){
+
+    fpp = fopen("/log.txt", "a+");                      /*创建文件用于输出log*/
+    if(!fpp){
     return 1;
-  }
+    }
 
-  /* register application */
-//  DLT_REGISTER_APP("Camera_Daemon","system camera daemon");
+    // memset(p_log, '\0', 300);
+    // sprintf(p_log, "START the main\n");
+    // fwrite(p_log, strlen(p_log), 1, fp);
+    // fflush(fp);
 
-  /* register all contexts */
-//  DLT_REGISTER_CONTEXT(Camera_Daemon,"CON1","Context 1 for Logging");
-
-  /* Start the nignx server*/
-//  system("service nginx start");
-
-  /* Start the janus webrtc gateway*/
-//  system("/opt/janus/bin/janus -F /opt/janus/etc/janus/");
-
-
-  CAMERACORE_log(fp,"[CAMERACORE_log]: main()  [START]\n");
-
-  /*Create the queue to store the command*/
-  DbustoCommand_queue = g_queue_new();
-  CommandtoFSM_queue  = g_queue_new();
-  FSMtoGst_queue      = g_queue_new();
-  SingaltoDbus_queue  = g_queue_new();
-
-
-  pthread_t gstPipeline,commandAdapter,fsm,sendsignal;
-   
-  
- 
+   print_log(fpp,"START the main\n");
   g_mNameRequestId = g_bus_own_name(G_BUS_TYPE_SYSTEM,
                                     CAMERADAE_SERVICE,
                                     G_BUS_NAME_OWNER_FLAGS_NONE,
@@ -89,53 +56,19 @@ int main(int argc, char *argv[]) {
                                     NULL,
                                     NULL); //GDestroyNotify
 
- 
-
-  //Create the threads
-  if( 0 != pthread_create(&gstPipeline, NULL, Gstreamer_Pipeline, NULL)){
-    CAMERACORE_log(fp,"[CAMERACORE_log]: main() [Failed to ctreate the thread : Gstreamer_Pipeline]\n");
-    //DLT_LOG(Camera_Daemon,DLT_LOG_ERROR,DLT_STRING("Failed to ctreate the thread : Gstreamer_Pipeline"));
-    return -1;
-  }
-  if( 0 != pthread_create(&commandAdapter, NULL, Command_Adapter, NULL)){
-    CAMERACORE_log(fp,"[CAMERACORE_log]: main() [Failed to ctreate the thread : Command_Adapter]\n");
-    //DLT_LOG(Camera_Daemon,DLT_LOG_ERROR,DLT_STRING("Failed to ctreate the thread : Command_Adapter"));
-    return -1;
-  }
-  if( 0 != pthread_create(&fsm, NULL, FSM, NULL)){
-    CAMERACORE_log(fp,"[CAMERACORE_log]: main() [Failed to ctreate the thread : FSM]\n");
-    //DLT_LOG(Camera_Daemon,DLT_LOG_ERROR,DLT_STRING("Failed to ctreate the thread : FSM"));
-    return -1;
-  }
-  if( 0 != pthread_create(&sendsignal, NULL, Send_Signal, NULL)){
-    CAMERACORE_log(fp,"[CAMERACORE_log]: main() [Failed to ctreate the thread : Send_Signal]\n");
-    //DLT_LOG(Camera_Daemon,DLT_LOG_ERROR,DLT_STRING("Failed to ctreate the thread : Send_Signal"));
-    return -1;
-  }
-
-
-
 
   p_loop = g_main_loop_new(NULL, TRUE);
   if(!p_loop) {
-    CAMERACORE_log(fp,"[CAMERACORE_log]: main() [Failed to create GMainLoop]\n");
-    
-    //DLT_LOG(OTACON1,DLT_LOG_ERROR,DLT_STRING("IVOS_CAMERACORE Failed to create GMainLoop"));
+    // memset(p_log, '\0', 300);
+    // sprintf(p_log, "IVOS_CAMERACORE Failed to create GMainLoop\n");
+    // fwrite(p_log, strlen(p_log), 1, fp);
+    // fflush(fp);
         return -2;
   }
   
   g_main_loop_run(p_loop);
   g_main_loop_unref(p_loop);
 
-  g_queue_free (DbustoCommand_queue);
-  g_queue_free (CommandtoFSM_queue);
-  g_queue_free (FSMtoGst_queue);
-  g_queue_free (SingaltoDbus_queue);
-  
-  /* unregister your contexts */
-  //DLT_UNREGISTER_CONTEXT(Camera_Daemon);
-
-  /* unregister your application */
-  //DLT_UNREGISTER_APP();
+ 
   return 0;  
 }
