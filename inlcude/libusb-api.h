3ef1dcbdfe4fcc83d1f42cdf6c46e63b67218712
Keerecles <keerecles@gmail.com>
2015-12-24 18:04:42 +0800

    libusb-api.c code

--- inlcude/libusb-api.h
+++ inlcude/libusb-api.h
@@ -10,3 +10,18 @@
 #include <errno.h>
 #include <libusb-1.0/libusb.h>
 #include "Cameradae.h" 
+
+
+
+
+int CAMERACORE_libusb_init();
+int CAMERACORE_libusb_hotplug_detect();
+int CAMERACORE_libusb_hotplug_cb();
+int CAMERACORE_libusb_device_info();
+int CAMERACORE_libusb_device_access();
+int CAMERACORE_libusb_device_mode();
+int CAMERACORE_libusb_update_deviceList();
+int CAMERACORE_libusb_device_connect();
+int CAMERACORE_libusb_data_feed();
+int CAMERACORE_libusb_transfer();
+int CAMERACORE_libusb_transfer_cb();
