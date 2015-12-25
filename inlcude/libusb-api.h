96b8541a2868d44a7cdd1b3baf69cdce6925c226
Keerecles <keerecles@gmail.com>
2015-12-25 18:06:18 +0800

    no

--- inlcude/libusb-api.h
+++ inlcude/libusb-api.h
@@ -3,16 +3,27 @@
 #include <string.h>
 #include <signal.h>
 #include <fcntl.h>
-#include <sys/types.h>
+#include <sys/types .h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <pthread.h>
 #include <errno.h>
+//#include <dlt/dlt.h>
 #include <libusb-1.0/libusb.h>
 #include "Cameradae.h" 
 
 
 
+struct Device
+{
+  uint8_t bus_number_;
+  uint8_t address_;
+  uint16_t vendor_id_;
+  uint16_t product_id_;
+  libusb_device_descriptor device_descriptor_;
+  libusb_device_handle* libusb_device_handle_;
+  libusb_device* libusb_device_;
+};
 
 int CAMERACORE_libusb_init();
 int CAMERACORE_libusb_hotplug_detect();
